cd0b66b5df609afbd7d9791d92bf08f25bbf9ac5
Yun LIU <yun.liu@edenred.com>
2019-03-07 18:18:57 +0800

    define Authenticate

---
+++ Cluster.Node/Authenticate/AuthenticatedGatewayFilter.cs
@@ -0,0 +1,34 @@
+﻿using Cluster.Node.Connection;
+using Cluster.Node.Filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cluster.Node.Authenticate
+{
+    public class AuthenticatedGatewayFilter : IGatewayFilter
+    {
+        private readonly IAuthenticateService authenticateService;
+
+        public AuthenticatedGatewayFilter(IAuthenticateService authenticateService)
+        {
+            this.authenticateService = authenticateService;
+        }
+
+        public List<ClusterNode> Filter(IConnectionToken token, List<ClusterNode> nodes)
+        {
+            IAuthenticatedConnectionToken authtoken = token as IAuthenticatedConnectionToken;
+            var list = new List<ClusterNode>();
+            foreach (var item in nodes)
+            {
+                if(this.authenticateService.Authenticate(item, authtoken))
+                {
+                    list.Add(item);
+                }
+            }            
+            return list;
+        }
+    }
+}
---
+++ Cluster.Node/Authenticate/IAuthenticateService.cs
@@ -0,0 +1,15 @@
+﻿using Cluster.Node.Connection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cluster.Node.Authenticate
+{
+    public interface IAuthenticateService
+    {
+        void Init();
+        bool Authenticate(ClusterNode node,IAuthenticatedConnectionToken token);
+    }
+}
